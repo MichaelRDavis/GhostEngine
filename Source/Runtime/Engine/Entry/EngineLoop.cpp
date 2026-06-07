60c8826c1f0cee44d036eabb4ed9cfd99012d55e
Michael Davis <MichaelRDavis1991@protonmail.com>
2026-06-07 14:29:11 +0100

    Added OpenGL context, window now displays clear color

--- Source/Runtime/Engine/Entry/EngineLoop.cpp
+++ Source/Runtime/Engine/Entry/EngineLoop.cpp
@@ -2,7 +2,15 @@
 #include <glad/glad.h>
 
 SDL_Window* window = nullptr;
+SDL_GLContext context = nullptr;
 bool isRunning = false;
+
+struct WindowProperties
+{
+	const char* title;
+	int width;
+	int height;
+};
 
 int main()
 {
@@ -13,10 +21,31 @@ int main()
 		return -1;
 	}
 
-	window = SDL_CreateWindow("Engine", 640, 480, 0);
+	WindowProperties winProps;
+	winProps.title = "Engine";
+	winProps.width = 1920;
+	winProps.height = 1080;
+
+	int windowFlags = SDL_WINDOW_OPENGL;
+	window = SDL_CreateWindow(winProps.title, winProps.width, winProps.height, windowFlags);
 	if (window == nullptr)
 	{
 		SDL_Log("Window could not be created! SDL error: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	context = SDL_GL_CreateContext(window);
+	if (context == nullptr)
+	{
+		SDL_Log("GL context could not be created! SDL error: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	SDL_GL_MakeCurrent(window, context);
+
+	if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
+	{
+		SDL_Log("glad could not load OpenGL! SDL error: %s\n", SDL_GetError());
 		return -1;
 	}
 
@@ -38,6 +67,11 @@ int main()
 				SDL_Quit();
 			}
 		}
+
+		glClearColor(0.3f, 0.7f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+		SDL_GL_SwapWindow(window);
 	}
 
 	return 0;
