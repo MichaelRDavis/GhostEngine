606d316a0a7ca4954b9fcd0e2d0eec4892f4e486
Michael Davis <MichaelRDavis1991@protonmail.com>
2026-06-08 22:39:22 +0100

    Removed test code

--- Source/Runtime/Engine/Entry/EngineLoop.c
+++ Source/Runtime/Engine/Entry/EngineLoop.c
@@ -1,38 +1,4 @@
-#include "OS/System.h"
-#include "Renderer/Renderer.h"
-
 int main()
 {
-	if (Sys_Init() != 0)
-	{
-		return -1;
-	}
-
-	Window window;
-	window.title = "Engine";
-	window.width = 1920;
-	window.height = 1080;
-	window.handle = 0;
-
-	App app;
-	app.bHasQuit = 0;
-	app.window = window;
-
-	if (Sys_CreateWindow(&app.window) != 0)
-	{
-		return -1;
-	}
-
-	RendererInfo info;
-	info.context = 0;
-	Renderer_CreateContext(&app.window, &info);
-
-	while (!app.bHasQuit)
-	{
-		Sys_Run(&app);
-		Renderer_Clear();
-		Renderer_SwapBuffers(&app.window);
-	}
-
 	return 0;
 }
